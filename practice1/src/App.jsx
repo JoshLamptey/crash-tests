86e6bfd3ea01790a0faee15819beae93a01dad59
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-07-27 00:33:08 -1200

    second

--- practice1/src/App.jsx
+++ practice1/src/App.jsx
@@ -4,55 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-// challenge 1
-// multiply the values of the numbers array by 6 using the map function
-
-const numbers = [9,67,98,2,3,4,90]
-
-// solution
-
-const multiplied = numbers.map((num)=>{
-
-  return num*6
-
-})
-//console.log(multiplied)
-
-// challenge 2 
-// capitalize the first letter of values of the array and reverse it
-
-const names = ["alice","baiana","makeba","tempoe","vicci"]
-
-//solution
-const capitalized = names.map((name)=>{
-  return name[0].toUpperCase()+name.slice(1)
-  }).reverse();
-
-//console.log(capitalized)
-//challenge 3
-// put the values in the action variables in an html <p></p>
-
-const action = ["pokemon","sonic","lara Croft","Tom Clancy","Simon Ghost Riley"]
-
-const sorted = action.map((games)=>{
-  return `<p>${games}</p>`
-})
-//console.log(sorted)
-
-const  colors =[
-  <h3>red</h3>,
-  <h3>violet</h3>,
-  <h3>yello</h3>,
-  <h3>gree</h3>,
-  <h3>blue</h3>,
-  <h3>indigo</h3>,
-  <h3>rainbow</h3>
-] 
-return(
-  <div>
-    {colors}
-  </div>
-)
 
 }
 
