4c87ff4ae0e36f0a41d082d7f0ef9dce38f4d4ac
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-08-16 23:43:39 -1200

    aecond

--- practice1/src/App.jsx
+++ practice1/src/App.jsx
@@ -1,29 +1,14 @@
 import Joke from './jokes'
 import jokesData from './jokesData'
 import './App.css'
-import { useState } from 'react'
+import Counter from './counter'
+import Ternary from './ternary'
 
 function App() {
-const [count,setCount] = useState(0)
-
-function Add(){
-  setCount((prevCount)=> prevCount +1)
-
-}
-
-function Minus(){
- setCount((prevCount)=> prevCount -1)
-}
   return (
-    <div className="counter">
-            <button onClick={Minus} className="counter--minus">-</button>
-            <div className="counter--count">
-                <h1>{count}</h1>
-            </div>
-            <button className="counter--plus" onClick={Add}>+</button>
-        </div>
-  )
-
+    <div>
+<Ternary />
+ </div> )
 }
 
 export default App
