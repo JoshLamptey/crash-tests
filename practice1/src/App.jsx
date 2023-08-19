5cc43916167310d8b561b1e664dd01bd05b89a30
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-08-19 03:38:54 -1200

    first

--- practice1/src/App.jsx
+++ practice1/src/App.jsx
@@ -3,11 +3,12 @@ import jokesData from './jokesData'
 import './App.css'
 import Counter from './counter'
 import Ternary from './ternary'
+import Add from './adding'
 
 function App() {
   return (
     <div>
-<Ternary />
+      <Add/>
  </div> )
 }
 
---
+++ practice1/src/adding.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+/*
+export default function Add() {
+    
+    const [thingsArray, setThingsArray] = useState(["Thing 1", "Thing 2"])
+    
+    function addItem() {
+        // We'll work on this next
+        
+        
+        setThingsArray((prevThingsArray) =>{ 
+       return [...prevThingsArray,`Thing ${prevThingsArray.length + 1}`] 
+        })
+        }
+    
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+    
+    return (
+        <div>
+            <button onClick={addItem}>Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}*/
+
+export default function Add(){
+    const [thingsArray,setThingsArray] = useState(["Thing 1", "Thing 2"])
+    
+    function addItem(){
+        setThingsArray((prevState)=>{
+            return [...prevState,`Thing ${prevState.length + 1}`]
+        })
+    }
+
+    const things = thingsArray.map(thing=>{
+     return   <p key={thing}>{thing}</p>
+})
+        
+    return (
+        <div>
+            <button onClick={addItem}>Add Item</button>
+            {things}
+        </div>
+    )
+}
