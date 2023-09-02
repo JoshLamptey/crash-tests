559d943b51698d3b1c83b81fcd91c7f8db17637d
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-09-01 21:17:23 -1200

    change

--- practice1/src/square.jsx
+++ practice1/src/square.jsx
@@ -3,16 +3,14 @@ import { useState } from "react"
 
 
 export default function App(props) {
-   const [on,setOn] = useState(props.on)
+   
 
-    const styles = on? {
+    const styles =props.on? {
         backgroundColor:"#222222"
     }:{
         backgroundColor:"#E9DCC9"
     }
-    function toggle(){
-        setOn(prevOn=>!prevOn)
-    }
+
     return (
         <div className="box" 
          onClick={()=>props.Click(props.id)} 
