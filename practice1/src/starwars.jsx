554dc4de032680536debf7f15c985461126878c4
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-09-27 04:12:36 -1200

    one and done

--- practice1/src/starwars.jsx
+++ practice1/src/starwars.jsx
@@ -5,12 +5,13 @@ export default function starWars(){
     const [count,setCount] =useState(0)
     console.log("component rendered")
    useEffect(function(){
-    console.log("effect ran")
+  fetch("https://swapi.dev/api/people/1")
+  .then(res=>res.json())
+  .then (data=>setStarWarsData(data))
    },[count])
         return(
             <div>
-               
-
+               <pre>{JSON.stringify(starWarsData,null,2)}</pre>
                 <h2>the count is {count}</h2>
                 <button onClick={()=>setCount(prevCount=> prevCount+1)}>add</button>
             </div>
