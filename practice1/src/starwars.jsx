9bbee52c3a0096249424dab553ac1f9d4165dc4b
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-09-29 06:31:39 -1200

    done

--- practice1/src/starwars.jsx
+++ practice1/src/starwars.jsx
@@ -2,22 +2,22 @@ import { useEffect, useState } from "react";
 
 export default function starWars(){
     const [starWarsData, setStarWarsData]= useState({})
-    const [count,setCount] =useState(0)
+    const [count,setCount] =useState(1)
    
 
 
 
    useEffect(function(){
     console.log("effect ran")
-  fetch("https://swapi.dev/api/people/1")
+  fetch(`https://swapi.dev/api/people/${count}`)
   .then(res=>res.json())
   .then (data=>setStarWarsData(data))
-   },[])
+   },[count])
         return(
             <div>
+                  <button onClick={()=>setCount(prevCount=> prevCount+1)}>Get The Next Character</button>
                <pre>{JSON.stringify(starWarsData,null,2)}</pre>
                 <h2>the count is {count}</h2>
-                <button onClick={()=>setCount(prevCount=> prevCount+1)}>add</button>
             </div>
         )
 
