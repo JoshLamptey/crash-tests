dff2c7271c12d8e1ebd7a7e71406e58f9fc4f0a3
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-09-28 12:28:55 -1200

    late night learning will take some dedication

--- practice1/src/starwars.jsx
+++ practice1/src/starwars.jsx
@@ -4,14 +4,15 @@ export default function starWars(){
     const [starWarsData, setStarWarsData]= useState({})
     const [count,setCount] =useState(0)
    
-    console.log("component rendered")
+
 
 
    useEffect(function(){
+    console.log("effect ran")
   fetch("https://swapi.dev/api/people/1")
   .then(res=>res.json())
   .then (data=>setStarWarsData(data))
-   },[count])
+   },[])
         return(
             <div>
                <pre>{JSON.stringify(starWarsData,null,2)}</pre>
