d9053cc7fda0ddd89f59929695d2738cf930249b
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-10-01 20:11:03 -1200

    one and done

--- practice1/src/windowTracker.jsx
+++ practice1/src/windowTracker.jsx
@@ -4,9 +4,18 @@ export default function WindowTracker() {
       const[windowWidth,setWindowWdith]= useState(window.innerWidth)
 
  useEffect(()=>{
- window.addEventListener("resize",function(){
+
+  function watchWidth(){
+    console.log("its mounted")
     setWindowWdith(window.innerWidth)
- })
+  }
+
+    window.addEventListener("resize",watchWidth)
+
+    return ()=>{
+        console.log('unmounted')
+        window.removeEventListener("resize",watchWidth)
+    }
  },[])
     return (
         <h1>Window width: {windowWidth}</h1>
